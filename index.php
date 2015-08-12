7106cef56702615d6a3c2df75a5e5ce50f5a3a71
Armando <armando.marisscal@gmail.com>
2015-08-12 07:13:35 -0600

    view

--- index.php
+++ index.php
@@ -1,43 +1,5 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="UTF-8">
-	<title><?= $title ?></title>
-	<link rel="stylesheet" href="css/main.css">
-	<link href='http://fonts.googleapis.com/css?family=Alice' rel='stylesheet' type='text/css'>
-</head>
-<body>
-	<div id="container">
-		<header>
-			<h1><?= $h1 ?></h1>
-			<h2><?= $h2 ?></h2>
-		</header>
-		<section>
-			<article>
-				<p>
-					<?= $p ?>
-				</p>
-			</article>
-			<aside>
-				<nav>
-					<h3><?= $h3 ?></h3>
-					<ul>
-					<?php
-
-						for ($i=1; $i < 6; $i++) { 
-
-							echo '<li>'	. $li[$i] . '</li>';
-
-						}
-
-					?>
-					</ul>
-				</nav>
-			</aside>
-		</section>
-		<footer>
-			<?= $footer ?>
-		</footer>
-	</div>
-</body>
-</html>
+<?php
+	include('v/v.head.phtml');
+	include('v/v.index.phtml');
+	include('v/v.footer.phtml');
+?>
