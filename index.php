cc47660d81bfe1d8fae775794b71810f2d2dde18
Armando <armando.marisscal@gmail.com>
2015-08-14 09:43:52 -0600

    views

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 	include('m/m.models.php');
 
 	# Views
-	include('v/v.head.phtml');
-	include('v/v.index.phtml');
-	include('v/v.footer.phtml');
+	include('v/v.views.phtml');
+	# include('v/v.head.phtml');
+	# include('v/v.index.phtml');
+	# include('v/v.footer.phtml');
 	
 ?>
